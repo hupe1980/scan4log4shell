e7d896ea2534f22f6456f46a402ea5e6ded6dfd2
hupe1980 <frankhuebner1980@gmail.com>
2021-12-18 14:13:21 +0100

    Extend testdata with more log4j versions

--- scripts/download-log4j.sh
+++ scripts/download-log4j.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 curl -L https://archive.apache.org/dist/logging/log4j/1.2.8/jakarta-log4j-1.2.8.tar.gz | tar xvz 
+curl -L https://archive.apache.org/dist/logging/log4j/2.12.1/apache-log4j-2.12.1-bin.tar.gz | tar xvz
+curl -L https://archive.apache.org/dist/logging/log4j/2.12.2/apache-log4j-2.12.2-bin.tar.gz | tar xvz  
 curl -L https://archive.apache.org/dist/logging/log4j/2.14.0/apache-log4j-2.14.0-bin.tar.gz | tar xvz 
 curl -L https://archive.apache.org/dist/logging/log4j/2.15.0/apache-log4j-2.15.0-bin.tar.gz | tar xvz 
 curl -L https://archive.apache.org/dist/logging/log4j/2.16.0/apache-log4j-2.16.0-bin.tar.gz | tar xvz 
+curl -L https://archive.apache.org/dist/logging/log4j/2.17.0/apache-log4j-2.17.0-bin.tar.gz | tar xvz 
